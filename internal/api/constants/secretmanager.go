caa6170cb4bc3442931066ff0575e45782c0e974
farid <farid.haikal@fast-8.com>
2023-09-22 00:48:32 +0700

    fix secret manager

--- internal/api/constants/secretmanager.go
+++ internal/api/constants/secretmanager.go
@@ -2,7 +2,7 @@ package constants
 
 const (
 	DbDialeg     = "db_dialeg"
-	DbHostWriter = "db_hostwriter"
+	DbHostWriter = "db_hostWriter"
 	DbHostReader = "db_hostReader"
 	DbPort       = "db_port"
 	DbName       = "db_name"
