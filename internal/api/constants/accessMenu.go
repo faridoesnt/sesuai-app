f3d5c6c0c609555a65459dd80d4c7e6ef1530835
faridoesnt <faridoesnt@gmail.com>
2023-10-09 01:03:27 +0700

    add constants access menu

---
+++ internal/api/constants/accessMenu.go
@@ -0,0 +1,19 @@
+package constants
+
+const (
+	GenerateToken = "Generate Token"
+	Element       = "Element"
+	QuestionList  = "Question List"
+	Submition     = "Submition"
+	PointAnswer   = "Point Answer"
+	AdminList     = "Admin List"
+)
+
+const (
+	EnumGenerateToken = "generate_token"
+	EnumElement       = "element"
+	EnumQuestionList  = "question_list"
+	EnumSubmition     = "submition"
+	EnumPointAnswer   = "point_answer"
+	EnumAdminList     = "admin_list"
+)
