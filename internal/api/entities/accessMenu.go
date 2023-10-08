67e0c34622130f1c78f5648a268a2f8cac3b0517
faridoesnt <faridoesnt@gmail.com>
2023-10-09 01:03:53 +0700

    update struct access menu

--- internal/api/entities/accessMenu.go
+++ internal/api/entities/accessMenu.go
@@ -1,8 +1,5 @@
 package entities
 
 type AccessMenu struct {
-	AccessId string `db:"access_id" json:"access_id"`
-	MenuId   string `db:"menu_id" json:"menu_id"`
-	MenuName string `db:"menu_name" json:"menu_name"`
-	Status   string `db:"status" json:"status"`
+	MenuName string `db:"menu_name"`
 }
