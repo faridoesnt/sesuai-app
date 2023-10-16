385aed4e5c5877d43e75b0bda17624e663bc2491
faridoesnt <faridoesnt@gmail.com>
2023-10-17 00:58:26 +0700

    add constants response

---
+++ internal/api/constants/response.go
@@ -0,0 +1,11 @@
+package constants
+
+const (
+	ResponseSuccess = "success"
+	ResponseFail    = "fail"
+	ResponseError   = "error"
+	ResponseTrue    = "true"
+	ResponseInvalid = "invalid"
+	ResponseValid   = "valid"
+	ResponseFalse   = "false"
+)
