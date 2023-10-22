14aec1809d3fefe7d862da454ee8f29f1a06cec9
faridoesnt <faridoesnt@gmail.com>
2023-10-23 00:04:26 +0700

    check admin has access menu in get questions by element id

--- internal/api/handlers/question.go
+++ internal/api/handlers/question.go
@@ -99,6 +99,19 @@ func GetQuestions(c iris.Context) {
 func GetAllQuestionsByElementId(c iris.Context) {
 	headers := helpers.GetHeaders(c)
 
+	adminId := c.Values().GetString(constants.AuthUserId)
+
+	hasAccess, err := app.Services.AccessMenu.IsAdminHasAccessMenu(adminId, constants.QuestionList)
+	if err != nil {
+		HttpError(c, headers, fmt.Errorf(err.Error()), ahttp.ErrFailure(err.Error()))
+		return
+	}
+
+	if !hasAccess {
+		HttpError(c, headers, fmt.Errorf("admin doesn't have access"), ahttp.ErrFailure("admin_doesn't_have_access"))
+		return
+	}
+
 	elementId := c.Params().GetString("elementId")
 
 	if elementId == "" {
