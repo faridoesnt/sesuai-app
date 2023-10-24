58fcca98820f3c483693c45d2bb32e32f1e5dbe5
faridoesnt <faridoesnt@gmail.com>
2023-10-24 16:20:25 +0700

    check admin has access menu in get submissions

--- internal/api/handlers/submission.go
+++ internal/api/handlers/submission.go
@@ -1,13 +1,28 @@
 package handlers
 
 import (
+	"Sesuai/internal/api/constants"
 	"Sesuai/internal/api/helpers"
 	"Sesuai/pkg/ahttp"
+	"fmt"
 	"github.com/kataras/iris/v12"
 )
 
 func GetSubmissions(c iris.Context) {
 	headers := helpers.GetHeaders(c)
+
+	adminId := c.Values().GetString(constants.AuthUserId)
+
+	hasAccess, err := app.Services.AccessMenu.IsAdminHasAccessMenu(adminId, constants.Submition)
+	if err != nil {
+		HttpError(c, headers, fmt.Errorf(err.Error()), ahttp.ErrFailure(err.Error()))
+		return
+	}
+
+	if !hasAccess {
+		HttpError(c, headers, fmt.Errorf("admin doesn't have access"), ahttp.ErrFailure("admin_doesn't_have_access"))
+		return
+	}
 
 	submissions := app.Services.Submission.GetSubmissions()
 
