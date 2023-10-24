eecc797a023df7203e7f453f81e9742efe4f0b03
faridoesnt <faridoesnt@gmail.com>
2023-10-24 16:40:24 +0700

    check admin has access menu in get blood type point

--- internal/api/handlers/bloodTypePoint.go
+++ internal/api/handlers/bloodTypePoint.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"Sesuai/internal/api/constants"
 	"Sesuai/internal/api/entities"
 	"Sesuai/internal/api/helpers"
 	"Sesuai/pkg/ahttp"
@@ -10,6 +11,19 @@ import (
 
 func GetBloodTypePoint(c iris.Context) {
 	headers := helpers.GetHeaders(c)
+
+	adminId := c.Values().GetString(constants.AuthUserId)
+
+	hasAccess, err := app.Services.AccessMenu.IsAdminHasAccessMenu(adminId, constants.Point)
+	if err != nil {
+		HttpError(c, headers, fmt.Errorf(err.Error()), ahttp.ErrFailure(err.Error()))
+		return
+	}
+
+	if !hasAccess {
+		HttpError(c, headers, fmt.Errorf("admin doesn't have access"), ahttp.ErrFailure("admin_doesn't_have_access"))
+		return
+	}
 
 	elementId := c.Params().GetString("elementId")
 
